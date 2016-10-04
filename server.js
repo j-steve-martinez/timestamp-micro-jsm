f301270752561d3092316cfb27bdeeb6ae378fef
J. Steve Martinez <j.steve.martinez@gmail.com>
2016-10-04 12:39:47 -0700

    removed references to mongodb

--- server.js
+++ server.js
@@ -4,25 +4,26 @@
 // User Story: If it does, it returns both the Unix timestamp and the natural language form of that date.
 // User Story: If it does not contain a date or Unix timestamp, it returns null for those properties.
 var express = require('express');
-var mongo = require('mongodb');
+// var mongo = require('mongodb');
 var routes = require('./app/routes/index.js');
 var app = express();
 
-mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
-
-   if (err) {
-      throw new Error('Database failed to connect!');
-   } else {
-      console.log('Successfully connected to MongoDB on port 27017.');
-   }
+// mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+//
+//    if (err) {
+//       throw new Error('Database failed to connect!');
+//    } else {
+//       console.log('Successfully connected to MongoDB on port 27017.');
+//    }
 
    app.use('/public', express.static(process.cwd() + '/public'));
   //  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-   routes(app, db);
+  //  routes(app, db);
+   routes(app);
 
    app.listen(3000, function () {
       console.log('Node.js listening on port 3000...');
    });
 
-});
+// });
